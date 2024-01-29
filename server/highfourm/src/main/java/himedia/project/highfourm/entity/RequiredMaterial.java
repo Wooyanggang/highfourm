53e3d68de409174069ddb2a2f0cdc7dc99975140
angdoo0 <143698028+angdoo0@users.noreply.github.com>
2024-01-29 12:40:31 +0900

    ♻️  RequiredMaterial.java

--- server/highfourm/src/main/java/himedia/project/highfourm/entity/RequiredMaterial.java
+++ server/highfourm/src/main/java/himedia/project/highfourm/entity/RequiredMaterial.java
@@ -2,6 +2,7 @@ package himedia.project.highfourm.entity;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
