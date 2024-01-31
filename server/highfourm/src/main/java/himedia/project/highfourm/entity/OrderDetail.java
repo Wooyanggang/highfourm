a00d354034d00d86cfaa079194b5a03e082e3376
Wooyanggang <didrod6589@naver.com>
2024-01-31 10:30:17 +0900

    🚨머지 후 에러 제거

--- server/highfourm/src/main/java/himedia/project/highfourm/entity/OrderDetail.java
+++ server/highfourm/src/main/java/himedia/project/highfourm/entity/OrderDetail.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
