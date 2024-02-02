e69ccf1beb338a672a2be8ab2487778abdaccb3f
lsj <tmdwnsrhc@gmail.com>
2024-02-03 06:26:31 +0900

    ✨ 제품별 공정/소요자재 단일개체 등록 기능 구현

--- server/highfourm/src/main/java/himedia/project/highfourm/repository/RequiredMaterialRepository.java
+++ server/highfourm/src/main/java/himedia/project/highfourm/repository/RequiredMaterialRepository.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import himedia.project.highfourm.dto.RequiredMaterialDTO;
 import himedia.project.highfourm.dto.bom.BomRequiredMaterialDTO;
+import himedia.project.highfourm.entity.RequiredMaterial;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -13,6 +17,20 @@ import lombok.RequiredArgsConstructor;
 public class RequiredMaterialRepository {
 	
 	private final EntityManager em;
+	
+	// save
+	@Transactional
+	public void save(RequiredMaterial requiredMaterial) {
+	    String sql = "INSERT INTO required_material (product_id, material_id, input_process, input_amount) " +
+	            "VALUES (:productId, :materialId, :inputProcess, :inputAmount)";
+
+	    em.createNativeQuery(sql)
+	            .setParameter("productId", requiredMaterial.getRequriedMaterialPK().getProduct().getProductId())
+	            .setParameter("materialId", requiredMaterial.getRequriedMaterialPK().getMaterial().getMaterialId())
+	            .setParameter("inputProcess", requiredMaterial.getInputProcess())
+	            .setParameter("inputAmount", requiredMaterial.getInputAmount())
+	            .executeUpdate();
+	}
 	
 	public List<BomRequiredMaterialDTO> findByProductId(String productId) {
 		String sql = "select rm.requriedMaterialPK.material.materialId, rm.inputProcess, rm.requriedMaterialPK.material.materialName, rm.inputAmount " 
