926c264967f6c7148640b7097142e8f1a82bab6e
lsj <tmdwnsrhc@gmail.com>
2024-02-03 06:27:03 +0900

    📝 누락 파일 추가 & 뷰 수정

--- frontend/src/components/View/Production/BomNew.js
+++ frontend/src/components/View/Production/BomNew.js
@@ -24,7 +24,11 @@ const BomNew = ({  onSubmit, onSubmitSuccess }) => {
     });
 
     const currentDate = new Date();
-    const formattedDate = currentDate.toLocaleDateString('ko-KR');
+    const year = currentDate.getFullYear();
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = currentDate.getDate().toString().padStart(2, '0');
+
+    const formattedDate = `${year}-${month}-${day}`;
     // 예제: BomRequestDTO와 일치하도록 구성
     const bomRequest = {
       productId: jsonData.productId,
